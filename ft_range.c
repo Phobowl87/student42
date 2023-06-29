d40dd2b31d48bf2677c60c5f2a1d9d6a69db9ca3
Phobowl87 <124695316+Phobowl87@users.noreply.github.com>
2023-06-29 11:36:49 +0200

    Update ft_range.c

--- ft_range.c
+++ ft_range.c
@@ -1,5 +1,12 @@
 #include <stdlib.h>
 
+int	ft_abs(int num)
+{
+	if (num < 0)
+		return (-num);
+	return (num);
+}
+
 int     *ft_range(int start, int end)
 {
 	int i = 0;
@@ -8,19 +15,20 @@ int     *ft_range(int start, int end)
   	res = (int *)malloc(sizeof(int) * len);
   
   	while (i < len)
-  	{
-    	  if (start < end)
-    	  {
-		  res[i] = start;
-		  start++;
-    		  i++;
-          }
-  	}
-  	else
-  	{
-    		res[i] = start;
-    		start--;
-    		i++;
-  	}
-  	return (res);
+	{
+		if (start < end)
+		{
+			res[i] = start;
+			start++;
+			i++;
+		}
+		else
+		{
+			res[i] = start;
+			start--;
+			i++;
+		}
+	}
+	return (res);
 }
+
